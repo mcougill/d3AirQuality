70415584c918e3d800bc9d44aa47902efce1cda3
Molly Cougill <molly.cougill@gmail.com>
2018-08-26 17:41:54 -0500

    data plotted

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,28 +3,32 @@ $(document).ready(function () {
     var apiKey = config.apiKey;
 
     //air quality index array
-    var aqiArray = [];
+
 
     var cityObj =
         [{
             "name": "Houston",
             "lat": "29.76",
-            "lon": "-95.37"
+            "lon": "-95.37",
+            "aqi": ""
         },
         {
             "name": "Los Angeles",
             "lat": "34.05",
-            "lon": "-118.24"
+            "lon": "-118.24",
+            "aqi": ""
         },
         {
             "name": "Chicago",
             "lat": "41.87957",
-            "lon": "-87.716406"
+            "lon": "-87.716406",
+            "aqi": "",
         },
         {
             "name": "New York City",
             "lat": "40.753",
-            "lon": "-87.716406"
+            "lon": "-87.716406",
+            "aqi": ""
         }
 
         ];
@@ -41,29 +45,74 @@ $(document).ready(function () {
             async: false
         })
             .done(function (response) {
-                aqiArray.push(response.data.current.pollution.aqius);
+                cityObj[i].aqi = response.data.current.pollution.aqius
             });
     }
 
-    console.log(aqiArray);
+    var cities = cityObj.map(function (d) {
+        return d.name
+    });
 
-    var margin = { left: 80, right: 20, top: 50, bottom: 100 };
+    var margin = { top: 5, right: 5, bottom: 50, left: 50 };
 
-    var width = 600 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
+    var fullWidth = 700;
+    var fullHeight = 200;
 
+    var width = fullWidth - margin.right - margin.left;
+    var height = fullHeight - margin.top - margin.bottom;
 
-    var g = d3.select("#chart-area")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
+    var svg = d3.select("#chart-area").append("svg")
+        .attr("width", fullWidth)
+        .attr("height", fullHeight)
         .append("g")
-        .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    var cityScale = d3.scaleBand()
+        .domain(cities)
+        .range([0, width])
+        .paddingInner(0.1);
 
+    var bandwidth = cityScale.bandwidth();
 
+    var maxAQI = d3.max(cityObj, function (d) {
+        return d.aqi
+    });
 
-        
+    var aqiScale = d3.scaleLinear()
+        .domain([0, maxAQI])
+        .range([height, 0])
+        .nice();
+
+    var xAxis = d3.axisBottom(cityScale);
+    var yAxis = d3.axisLeft(aqiScale);
+
+    svg.append("g")
+        .classed("x axis", true)
+        .attr("transform", "translate(0" + height + ")")
+        .call(xAxis);
+
+    var yAxisEle = svg.append("g")
+        .classed("y axis", true)
+        .call(yAxis)
+
+    var barHolder = svg.append("g")
+        .classed("bar-holder", true);
+
+    var bars = barHolder.selectAll("rect.bar")
+        .data(cityObj)
+        .enter().append("rect")
+        .classed("bar", true)
+        .attr("x", function (d, i) {
+            return cityScale(d.name)
+        })
+        .attr("width", bandwidth)
+        .attr("y", function (d) {
+            return aqiScale(d.aqi);
+        })
+        .attr("height", function (d) {
+            return height - aqiScale(d.aqi);
+        });
+
 });
 
 
